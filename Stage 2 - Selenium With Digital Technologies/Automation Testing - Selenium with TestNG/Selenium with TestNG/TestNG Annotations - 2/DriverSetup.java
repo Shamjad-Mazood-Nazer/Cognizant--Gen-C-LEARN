a4b4ab6fed9a5b224424372b48b1a6eae70a0aa1
Shamjad-Mazood-Nazer <shamjadmazoodnazer2023b@mca.ajce.in>
2024-05-11 23:32:09 +0530

    Automation Testing - Selenium with TestNG

---
+++ Stage 2 - Selenium With Digital Technologies/Automation Testing - Selenium with TestNG/Selenium with TestNG/TestNG Annotations - 2/DriverSetup.java
@@ -0,0 +1,38 @@
+package Stage 2 - Selenium With Digital Technologies.Automation Testing - Selenium with TestNG.Selenium with TestNG.TestNG Annotations - 2;
+
+public /* IMPORTANT:- DriverSetup --getDriver()
+-------------------------------------------------
+PLEASE DO NOT MAKE ANY CHANGES OR MOFICATIONS IN THIS PROGRAM */
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+public class DriverSetup
+{ 
+    private static WebDriver driver;
+    public static WebDriver getWebDriver()
+	{
+		System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+		FirefoxBinary firefoxBinary = new FirefoxBinary();
+		firefoxBinary.addCommandLineOptions("--headless");
+	    FirefoxProfile profile=new FirefoxProfile();
+	    //profile.setPreference("marionette.logging", "TRACE");
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setBinary(firefoxBinary);
+		firefoxOptions.setProfile(profile);
+	    driver=new FirefoxDriver(firefoxOptions);
+	    return driver;
+	}
+}
+
+ {
+    
+}
